f4d8dfa8182722b963afe0b236e60135bd50d406
katelincatton <119131202+katelincatton@users.noreply.github.com>
2023-01-25 15:41:01 -0700

    Final

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -56,3 +56,64 @@ CREATE TABLE salaries (
 --View table data
 SELECT *
 FROM departments
+
+SELECT * FROM dept_emp
+
+
+---Deliverable 1
+--Table 1
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- Table 2
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirees_titles AS rt
+ORDER BY emp_no, to_date DESC;
+
+--Table 3
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--DELIVERABLE 2
+
+--The Employees Eligible for the Mentorship Program.
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.from_date DESC
+
